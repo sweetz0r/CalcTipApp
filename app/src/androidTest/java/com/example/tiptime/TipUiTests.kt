f276655164f4a06d31401b41ef04b054eeca3960
Gleb BIller <sweetzr16@gmail.com>
2024-03-20 22:14:32 +0300

    added tests

---
+++ app/src/androidTest/java/com/example/tiptime/TipUiTests.kt
@@ -0,0 +1,11 @@
+package com.example.tiptime
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+}
